51c8a3b2899cd8e5c20590161ebf5e2207089259
Satianurag <hsingh.hs.hs47@gmail.com>
2023-12-14 18:32:48 +0530

    -all

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ async function getWeather() {
     }
 
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
-
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
